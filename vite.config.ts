c58e9932e4702eea37f526b1346da63e2d4190a9
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-01-11 13:17:33 -0500

    build: :package: Add million package and configure Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import million from 'million/compiler';
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [million.vite({ auto: true }), remix(), tsconfigPaths()],
 });
