397f3e9b1b97f3a838cf3d97286f035d15fad4c1
CJ Rose <cjrose59@gmail.com>
2024-01-14 12:48:24 -0700

    add font and custom colors to tailwind config

--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,10 @@ export default function App() {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200..700&display=swap"
+        />
 				<Meta />
 				<Links />
 			</head>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,41 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans:["Plus Jakarta Sans",]
+      },
+      colors: {
+        // main text 'CloudBurst'
+        textBlue: "#252F56",
+        // page background 'DawnPink'
+        beige200: "#F7EDE8",
+        // container backgrounds
+        beige400: "#E7C9BA",
+        beige300: "#F1E0D7",
+        beige100: "#FBF7F5",        
+        // accent 'Gothic'
+        blue700: "#3A5264",
+        blue600: "#416279",
+        blue500: "#4B778F",
+        blue400: "#6692A9",
+        blue300: "#98B8C8",
+        blue200: "#C2D6DF",
+        // accent 'ElSalva'
+        orange800: "#87362D",
+        orange700: "#9F3D32",
+        orange600: "#C14E41",
+        orange500: "#D6695D",
+        orange400: "#E48F85",
+        orange300: "#EFB8B2",
+        orange200: "#F6D6D2",
+        // accent 'Woodsmoke'
+        gray600: "#42424A",
+        gray500: "#5F5F6A",
+        gray400: "#92939E",
+        gray300: "#B9BAC0",
+        gray200: "#DADADD",
+
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
