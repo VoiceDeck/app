7298ef1c2418cd3be0a49a1cb5eb1b2df47e9ded
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-01-14 22:03:58 -0500

    feat: :sparkles: Add route for displaying individual reports

---
+++ app/routes/reports.$slug.tsx
@@ -0,0 +1,61 @@
+import type { LoaderFunctionArgs, MetaArgs } from "@remix-run/node";
+import { MetaFunction, useLoaderData } from "@remix-run/react";
+
+export interface ReportSchema {
+	id: string;
+	status: string;
+	date_created: string;
+	title: string;
+	content: string;
+	slug: string;
+}
+
+export const meta: MetaFunction = ({ data }: MetaArgs) => {
+	const report = data as ReportSchema;
+	return [{ title: `VoiceDeck | ${report.title}` }];
+};
+
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+	const slug = params.slug;
+	if (!slug) {
+		return {
+			status: 404,
+			error: "Report not found",
+		};
+	}
+	try {
+		const response = await fetch(
+			`${process.env.API_BASE_URL}/items/report?filter[slug][_eq]=${slug}`,
+			{
+				headers: {
+					Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
+				},
+			},
+		);
+		const json = await response.json();
+		if (json.data.length === 0) {
+			return {
+				status: 404,
+				error: "Report not found",
+			};
+		}
+		return json.data[0];
+	} catch (error) {
+		console.error(error);
+		return {
+			status: 500,
+			error: "Failed to load report",
+		};
+	}
+};
+
+export default function RouteComponent() {
+	const loaderResponse = useLoaderData<typeof loader>();
+	const report = loaderResponse as ReportSchema;
+	return (
+		<article>
+			<h1>{report.title}</h1>
+			<p className="prose">{report.content}</p>
+		</article>
+	);
+}
