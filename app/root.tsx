83cfed2074378863378269a63e1a0a7dd7c0415a
CJ Rose <cjrose59@gmail.com>
2024-01-25 19:28:52 -0700

    move HC loading into root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import "@fontsource-variable/plus-jakarta-sans";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import {
 	Link,
 	Links,
@@ -7,12 +9,43 @@ import {
 	Scripts,
 	ScrollRestoration,
 	isRouteErrorResponse,
+	json,
 	useRouteError,
+	useRouteLoaderData,
 } from "@remix-run/react";
-
-import "@fontsource-variable/plus-jakarta-sans";
 import { ReactNode } from "react";
+import { fetchReports } from "~/server/impactReportHelpers";
 import "./tailwind.css";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const ownerAddress = process.env.HC_OWNER_ADDRESS;
+	try {
+		if (!ownerAddress) {
+			throw new Error("Owner address environment variable is not set");
+		}
+		const reports = await fetchReports(ownerAddress);
+		// remix shortcut to return json instead of a string
+		return json(reports);
+		// return new Response((reports), {
+		// 	status: 200,
+		// 	statusText: "OK",
+		// });
+	} catch (error) {
+		console.error(`Failed to load impact reports: ${error}`);
+		return new Response(
+			JSON.stringify({ error: "Failed to load impact reports" }),
+			{
+				status: 500,
+				statusText: "Internal Server Error",
+			},
+		);
+	}
+};
+
+// custom hook to pass the loader's type to other routes
+export function useRootLoaderData() {
+	return useRouteLoaderData<typeof loader>("root");
+}
 
 export default function App() {
 	return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,32 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useRootLoaderData } from "~/root";
+import { Report } from "~/types";
 
 export const meta: MetaFunction = () => {
 	return [
-		{ title: "New Remix App" },
-		{ name: "description", content: "Welcome to Remix!" },
+		{ title: "VoiceDeck" },
+		{ name: "description", content: "Welcome to VoiceDeck!" },
 	];
 };
 
 export default function Index() {
+	const reports = useRootLoaderData();
 	return (
-		<div>
-			<h1 className="text-7xl">Welcome to Remix</h1>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
-					>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-						Remix Docs
-					</a>
-				</li>
-			</ul>
+		<div className="flex flex-col gap-10">
+			<h1 className="text-7xl">VoiceDeck</h1>
+			<h2 className="text-5xl">Reports</h2>
+			{reports.map((report: Report) => (
+				<div key={report.id}>
+					<div>{report.id}</div>
+					<div>{report.title}</div>
+					<div>{report.summary}</div>
+					<div>{report.image.slice(0, 50)}</div>
+					<div>{report.state}</div>
+					<div>{report.category}</div>
+					<div>{report.totalCost}</div>
+					<div>{report.fundedSoFar}</div>
+				</div>
+			))}
 		</div>
 	);
 }
