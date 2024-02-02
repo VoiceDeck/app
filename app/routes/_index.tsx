cb28a25e203b9d58fe44db94f2b1e9328a0fa148
CJ Rose <cjrose59@gmail.com>
2024-02-02 11:17:42 -0700

    resize masthead elements

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -78,12 +78,12 @@ export const loader: LoaderFunction = async () => {
 export default function Index() {
 	const reports = useLoaderData<typeof loader>();
 	return (
-		<main className="flex flex-col gap-8 md:gap-10 justify-center items-center p-4 md:px-[15%]">
-			<header className="flex-row bg-[url('/hero_img.jpg')] bg-cover bg-center justify-start items-baseline text-vd-beige-200 rounded-3xl p-6 pt-44 md:pt-80 md:pr-48 md:pb-10 md:pl-16 max-w-[1372px]">
-				<h1 className="text-6xl md:text-7xl font-bold text-left">
-					From Individual Actions to Collective Impact
+		<main className="flex flex-col gap-8 md:gap-6 justify-center items-center p-4 md:px-[15%]">
+			<header className="flex-row bg-[url('/hero_imgLG.jpg')] bg-cover bg-center justify-start items-baseline text-vd-beige-200 rounded-3xl p-4 pt-24 md:pt-36 md:pr-48 md:pb-2 md:pl-8 max-w-[1372px]">
+				<h1 className="text-6xl font-bold text-left">
+					From individual actions to collective impact
 				</h1>
-				<h2 className="text-xl font-medium text-left py-6">
+				<h2 className="text-lg font-medium text-left py-6">
 					We enable journalists to effect real change by bringing critical
 					stories to light. Your contributions directly support this mission,
 					sustaining journalism and bolstering investigative reporting that
@@ -91,38 +91,36 @@ export default function Index() {
 				</h2>
 			</header>
 
-			<section className="flex flex-col lg:flex-row w-full gap-3 lg:gap-5 max-w-[1372px]">
-				<div className="flex flex-auto gap-8 lg:w-[33%] rounded-3xl bg-vd-blue-200 p-4">
+			<section className="flex flex-col lg:flex-row w-full gap-3 lg:gap-3 max-w-[1372px]">
+				<div className="flex flex-auto items-center gap-4 lg:w-[33%] rounded-3xl bg-vd-blue-200 p-4">
 					<img src={"/blue_flower.svg"} alt="blue flower drawing" />
-					<div className="flex flex-col gap-4">
-						<p className="text-xl font-medium">Total Supporters</p>
-						<p className="text-4xl md:text-5xl font-bold">104</p>
+					<div className="flex flex-col gap-2">
+						<p className="text-base font-medium">Total Supporters</p>
+						<p className="text-3xl md:text-3xl font-bold">104</p>
 					</div>
 				</div>
-				<div className="flex flex-auto gap-8 lg:w-[33%] rounded-3xl bg-vd-blue-200 p-4">
+				<div className="flex flex-auto items-center gap-4 lg:w-[33%] rounded-3xl bg-vd-blue-200 p-4">
 					<img src={"/blue_elephant.svg"} alt="blue elephant drawing" />
-					<div className="flex flex-col gap-4">
-						<p className="text-xl font-medium">Total Support Received</p>
-						<p className="text-4xl md:text-5xl font-bold">
-							3.6K <span className="text-xl">USD</span>
+					<div className="flex flex-col gap-2">
+						<p className="text-base font-medium">Total Support Received</p>
+						<p className="text-3xl md:text-3xl font-bold">
+							3.6K <span className="text-lg">USD</span>
 						</p>
 					</div>
 				</div>
-				<div className="flex flex-auto gap-8 lg:w-[33%] rounded-3xl bg-vd-blue-200 p-4">
+				<div className="flex flex-auto items-center gap-4 lg:w-[33%] rounded-3xl bg-vd-blue-200 p-4">
 					<img src={"/blue_candle.svg"} alt="blue candle drawing" />
-					<div className="flex flex-col gap-4">
-						<p className="text-xl font-medium"># of Reports Fully Funded</p>
-						<p className="text-4xl md:text-5xl font-bold">12</p>
+					<div className="flex flex-col gap-2">
+						<p className="text-base font-medium"># of Reports Fully Funded</p>
+						<p className="text-3xl md:text-3xl font-bold">12</p>
 					</div>
 				</div>
 			</section>
 
 			<article className="w-full max-w-[1372px]">
-				<h2 className="text-4xl md:text-5xl font-bold pt-12 md:pt-16 pb-3">
-					Reports
-				</h2>
-				<div className="flex flex-col md:flex-row md:justify-between md:items-end pb-12">
-					<p className="text-xl pb-4 md:pb-0 ">
+				<h2 className="text-3xl md:text-4xl font-semibold pt-6">Reports</h2>
+				<div className="flex flex-col md:flex-row md:justify-between md:items-end pb-8">
+					<p className="text-base pb-4 md:pb-0 ">
 						Find and fund reports that resonate with you.
 					</p>
 					<div className="flex flex-col md:flex-row gap-3">
