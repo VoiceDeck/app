45a0d1684baa72c451c1cc5119faa869bed2ea05
Rowdy <mroddy5280@gmail.com>
2024-02-06 09:50:33 -0700

    feat(footer.tsx,-config/site.ts): moved links to config, finished desktop styles

--- app/components/global/footer.tsx
+++ app/components/global/footer.tsx
@@ -11,51 +11,61 @@ import {
 	DrawerTitle,
 	DrawerTrigger,
 } from "~/components/ui/drawer";
+import { externalLinks } from "~/config/site";
 import { cn } from "~/lib/utils";
 import logo from "/logo.svg";
 
 const Footer = () => {
 	return (
-		<footer className="fixed inset-x-0 bottom-0 bg-red-100 text-center items-center py-2">
-			<ul className="flex justify-around items-center gap-1 md:hidden">
-				<li>
-					<NavLink
-						to="/"
-						className={cn(buttonVariants({ variant: "ghost", size: "icon" }))}
-					>
+		<footer className="sticky inset-x-0 bottom-0 bg-red-100 text-center items-center py-2 md:static">
+			<MobileFooter />
+			<DesktopFooter />
+		</footer>
+	);
+};
+
+const MobileFooter = () => {
+	return (
+		<ul className="flex justify-around items-center gap-1 md:hidden">
+			<li>
+				<NavLink
+					to="/"
+					className={cn(buttonVariants({ variant: "ghost", size: "icon" }))}
+				>
+					<div className="flex flex-col justify-center items-center">
+						<Newspaper />
+						<span className="text-xs">Reports</span>
+					</div>
+				</NavLink>
+			</li>
+			<li>
+				<NavLink
+					to="/actions"
+					className={cn(buttonVariants({ variant: "ghost", size: "icon" }))}
+				>
+					<div className="flex flex-col justify-center items-center">
+						<HeartHandshake />
+						<span className="text-xs">My Actions</span>
+					</div>
+				</NavLink>
+			</li>
+			<li>
+				<Drawer>
+					<DrawerTrigger>
 						<div className="flex flex-col justify-center items-center">
-							<Newspaper />
-							<span className="text-xs">Reports</span>
+							<Menu />
+							<span className="text-xs">More</span>
 						</div>
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-						to="/actions"
-						className={cn(buttonVariants({ variant: "ghost", size: "icon" }))}
-					>
-						<div className="flex flex-col justify-center items-center">
-							<HeartHandshake />
-							<span className="text-xs">My Actions</span>
-						</div>
-					</NavLink>
-				</li>
-				<li>
-					<Drawer>
-						<DrawerTrigger>
-							<div className="flex flex-col justify-center items-center">
-								<Menu />
-								<span className="text-xs">More</span>
+					</DrawerTrigger>
+					<DrawerContent className=" gap-10">
+						<DrawerHeader>
+							<div className="flex justify-center">
+								<img className="h-10 w-10" src={logo} alt="VoiceDeck Logo" />
 							</div>
-						</DrawerTrigger>
-						<DrawerContent className=" gap-10">
-							<DrawerHeader>
-								<div className="flex justify-center">
-									<img className="h-10 w-10" src={logo} alt="VoiceDeck Logo" />
-								</div>
-							</DrawerHeader>
+						</DrawerHeader>
+						{externalLinks.map((link) => (
 							<a
-								href="http://about-example.com"
+								href={link.url}
 								target="_blank"
 								rel="noopener noreferrer"
 								className={cn(
@@ -63,57 +73,57 @@ const Footer = () => {
 									"text-lg justify-between",
 								)}
 							>
-								About
+								{link.title}
 								<ArrowUpRight size={24} />
 							</a>
-							<a
-								href="http://about-example.com"
-								target="_blank"
-								rel="noopener noreferrer"
-								className={cn(
-									buttonVariants({ variant: "link" }),
-									"text-lg justify-between",
-								)}
-							>
-								FAQs <ArrowUpRight size={24} />
-							</a>
-							<a
-								href="http://about-example.com"
-								target="_blank"
-								rel="noopener noreferrer"
-								className={cn(
-									buttonVariants({ variant: "link" }),
-									"text-lg justify-between",
-								)}
-							>
-								GitHub <ArrowUpRight size={24} />
-							</a>
-							<a
-								href="http://about-example.com"
-								target="_blank"
-								rel="noopener noreferrer"
-								className={cn(
-									buttonVariants({ variant: "link" }),
-									"text-lg justify-between",
-								)}
-							>
-								X <ArrowUpRight size={24} />
-							</a>
-							<DrawerFooter>
-								<DrawerClose>
-									<Button variant="ghost" size="icon">
-										<div className="flex flex-col justify-center items-center">
-											<X />
-											<span className="text-xs">Close</span>
-										</div>
-									</Button>
-								</DrawerClose>
-							</DrawerFooter>
-						</DrawerContent>
-					</Drawer>
-				</li>
+						))}
+
+						<DrawerFooter>
+							<DrawerClose>
+								<Button variant="ghost" size="icon">
+									<div className="flex flex-col justify-center items-center">
+										<X />
+										<span className="text-xs">Close</span>
+									</div>
+								</Button>
+							</DrawerClose>
+						</DrawerFooter>
+					</DrawerContent>
+				</Drawer>
+			</li>
+		</ul>
+	);
+};
+
+const DesktopFooter = () => {
+	return (
+		<div className="hidden md:flex md:container h-32 items-center justify-between">
+			<div className="flex flex-col">
+				<div className="flex items-center">
+					<img className="h-10 w-10" src={logo} alt="VoiceDeck Logo" />
+					<h3 className="text-5xl font-semibold">VoiceDeck</h3>
+				</div>
+				<small className="text-base">
+					An Open Source project. A funding platform for the collective impact.
+				</small>
+			</div>
+			<ul>
+				{externalLinks.map((link) => (
+					<a
+						href={link.url}
+						target="_blank"
+						rel="noopener noreferrer"
+						className={cn(
+							buttonVariants({ variant: "link" }),
+							"text-lg justify-between",
+						)}
+					>
+						{link.title}
+						<ArrowUpRight size={24} />
+					</a>
+				))}
 			</ul>
-		</footer>
+		</div>
 	);
 };
 
--- app/config/site.ts
+++ app/config/site.ts
@@ -22,3 +22,11 @@ export const siteConfig: SiteConfig = {
 		github: "https://github.com/VoiceDeck/",
 	},
 };
+
+// TODO: Clean up, add icon, add to sight config?
+export const externalLinks = [
+	{ title: "About", url: "http://about-example.com" },
+	{ title: "FAQs", url: "http://faqs-example.com" },
+	{ title: "GitHub", url: "https://github.com/VoiceDeck/" },
+	{ title: "X", url: "https://twitter.com/VoiceDeckDAO" },
+];
