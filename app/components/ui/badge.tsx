29006b6e6790ecf75a707a666433769dff446976
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-02-08 13:34:53 -0500

    refactor: :recycle: Update badge styles

--- app/components/ui/badge.tsx
+++ app/components/ui/badge.tsx
@@ -4,14 +4,14 @@ import * as React from "react";
 import { cn } from "~/lib/utils";
 
 const badgeVariants = cva(
-	"inline-flex gap-1 items-center rounded-full border border-vd-orange-800 px-2 py-1 text-[10px] font-normal transition-colors focus:outline-none focus:ring-2 focus:ring-vd-orange-800 focus:ring-offset-2 dark:vd-beige-100 dark:focus:ring-vd-beige-100",
+	"inline-flex gap-1 items-center rounded-full border border-vd-orange-800 px-2 py-1 text-xs font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-vd-orange-800 focus:ring-offset-2 dark:vd-beige-100 dark:focus:ring-vd-beige-100",
 	{
 		variants: {
 			variant: {
 				default:
-					"border-transparent bg-vd-beige-300 text-vd-orange-900 hover:bg-vd-beige-300/70 dark:bg-beige-300 dark:text-vd-orange-900 dark:hover:bg-beige-300/70",
+					"border-transparent bg-vd-beige-100 text-vd-orange-800 hover:bg-vd-beige-300/70 dark:bg-beige-300 dark:text-vd-orange-900 dark:hover:bg-beige-300/70",
 				secondary:
-					"border-transparent bg-vd-beige-100 text-vd-orange-900 hover:bg-vd-beige-100 dark:bg-vd-beige-100 dark:text-vd-orange-900 dark:hover:bg-vd-beige-100",
+					"border-transparent bg-vd-beige-100 text-vd-orange-700 hover:bg-vd-beige-100 dark:bg-vd-beige-100 dark:text-vd-orange-900 dark:hover:bg-vd-beige-100",
 				destructive:
 					"border-transparent bg-red-500 text-vd-beige-100 hover:bg-red-500/80 dark:bg-red-900 dark:text-vd-beige-100 dark:hover:bg-red-900/80",
 				outline: "text-vd-orange-900 dark:text-vd-orange-900",
