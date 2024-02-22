da9d34fbc3c557bac37d88624db834837e2e6e3e
CJ Rose <cjrose59@gmail.com>
2024-02-22 12:30:03 -0700

    add checkbox to filters

--- app/components/reports/reports-filters.tsx
+++ app/components/reports/reports-filters.tsx
@@ -1,6 +1,6 @@
-import { Separator } from "@radix-ui/react-separator";
-import { Circle, Filter } from "lucide-react";
+import { Filter } from "lucide-react";
 import { Button } from "~/components/ui/button";
+import { Checkbox } from "~/components/ui/checkbox";
 import {
 	Dialog,
 	DialogClose,
@@ -15,6 +15,7 @@ import {
 	DrawerFooter,
 	DrawerTrigger,
 } from "~/components/ui/drawer";
+import { Separator } from "~/components/ui/separator";
 import { Slider } from "~/components/ui/slider";
 
 interface ReportFiltersProps {
@@ -33,7 +34,7 @@ const FilterItems: React.FC<ReportFiltersProps> = ({
 	const rangeBetweenAmounts = maxAmountNeeded - minAmountNeeded;
 	return (
 		<div className="p-6">
-			<h2 className="pl-4 font-medium">Amount needed</h2>
+			<h2 className="pl-4 text-lg font-medium">Amount needed</h2>
 			<div className="px-6 py-8">
 				<Slider
 					defaultValue={[
@@ -46,24 +47,31 @@ const FilterItems: React.FC<ReportFiltersProps> = ({
 					minStepsBetweenThumbs={18}
 				/>
 			</div>
-			<div className="w-full h-[1px] bg-vd-blue-900 my-6 md:my-10" />
+			<Separator className="bg-vd-blue-500 my-6 md:my-10" />
 			<div className="flex flex-col md:flex-row md:justify-between">
 				<div className="pl-4">
-					<h2 className="font-medium pb-2 md:pb-4">Story from</h2>
+					<h2 className="text-lg font-medium pb-2 md:pb-4">Story from</h2>
 					{outlets.map((outlet: string) => (
-						<div key={outlet} className="flex items-center gap-2 pb-1">
-							<Circle size={18} strokeWidth={1} />
-							<p className="text-xs">{outlet}</p>
+						<div key={outlet} className="flex items-center gap-2 pb-2">
+							<Checkbox className="h-6 w-6 rounded-full border-vd-blue-500 data-[state=checked]:bg-vd-blue-500 data-[state=checked]:text-vd-beige-100" />
+							<p className="text-base">{outlet}</p>
 						</div>
 					))}
 				</div>
-				<div className="md:hidden w-full h-[1px] bg-vd-blue-900 my-6" />
+				<Separator
+					className="md:hidden bg-vd-blue-500 my-6"
+					orientation="horizontal"
+				/>
+				<Separator
+					className="hidden md:flex bg-vd-blue-500 mx-6"
+					orientation="vertical"
+				/>
 				<div className="pl-4">
-					<h2 className="font-medium pb-2 md:pb-4">State</h2>
+					<h2 className="text-lg font-medium pb-2 md:pb-4">State</h2>
 					{states.map((state: string) => (
-						<div key={state} className="flex items-center gap-2 pb-1">
-							<Circle size={18} strokeWidth={1} />
-							<p className="text-xs">{state}</p>
+						<div key={state} className="flex items-center gap-2 pb-2">
+							<Checkbox className="h-6 w-6 rounded-full border-vd-blue-500 data-[state=checked]:bg-vd-blue-500 data-[state=checked]:text-vd-beige-100" />
+							<p className="text-base">{state}</p>
 						</div>
 					))}
 				</div>
@@ -82,8 +90,8 @@ const ReportsFilters: React.FC<ReportFiltersProps> = ({
 			<div className="md:hidden">
 				<Drawer>
 					<DrawerTrigger className="flex gap-3 h-10 w-full rounded-md border-input justify-between items-center bg-vd-beige-100 border border-vd-blue-500 px-3 py-2">
-						<p className="text-base font-medium text-vd-blue-500">Filter</p>
-						<Filter color="#3A5264" size={18} />
+						<p className="text-sm font-medium text-vd-blue-500">Filter</p>
+						<Filter color="#4B778F" size={18} />
 					</DrawerTrigger>
 					<DrawerContent className="">
 						<FilterItems outlets={outlets} states={states} amounts={amounts} />
@@ -110,7 +118,7 @@ const ReportsFilters: React.FC<ReportFiltersProps> = ({
 						<p className="text-sm font-medium text-vd-blue-500">Filter</p>
 						<Filter color="#4B778F" size={18} />
 					</DialogTrigger>
-					<DialogContent>
+					<DialogContent className="bg-vd-beige-200">
 						<FilterItems outlets={outlets} states={states} amounts={amounts} />
 						<DialogFooter className="justify-end gap-2 pb-4">
 							<Button
