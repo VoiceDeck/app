c1b26159586511807c5d249dce006b013e21d952
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-02-29 23:30:11 -0500

    chore(config): :wrench: Refactor entry.client.tsx and vite.config.ts

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,15 +1,12 @@
 import { RemixBrowser } from "@remix-run/react";
 import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
-import RootProvider from "./providers/root-provider";
 
 startTransition(() => {
 	hydrateRoot(
 		document,
 		<StrictMode>
-			<RootProvider>
-				<RemixBrowser />
-			</RootProvider>
+			<RemixBrowser />
 		</StrictMode>,
 	);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import million from "million/compiler";
+import { installGlobals } from "@remix-run/node";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { remixDevTools } from "remix-development-tools/vite";
+
+installGlobals();
 
 export default defineConfig({
-  plugins: [million.vite({auto: true}), remix(), tsconfigPaths()],
+  plugins: [
+    remixDevTools(),
+    million.vite({ auto: true }),
+    remix(),
+    tsconfigPaths(),
+  ],
   test: {
     include: ["./app/**/*.test.ts"],
     reporters: ["verbose"],
