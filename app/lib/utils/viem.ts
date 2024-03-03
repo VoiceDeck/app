27e4c943f324147983ab8154f19f9eda8c5d9049
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-02 20:17:19 -0500

    feat: :wrench: Update viem.ts with new RPC URLs

--- app/lib/utils/viem.ts
+++ app/lib/utils/viem.ts
@@ -1,28 +1,28 @@
 import { http, createPublicClient, getContract } from "viem";
-import { mainnet, optimismSepolia } from "viem/chains";
+import { mainnet, sepolia } from "viem/chains";
 import { multivaultAbi } from "../abis/ethMultiVault";
 import { MULTIVAULT_CONTRACT_ADDRESS } from "./constants";
 
-const alchemyRpcUrl = process.env.ALCHEMY_RPC_URL;
-const alchemyMainnetRpcUrl = process.env.ALCHEMY_MAINNET_RPC_URL;
+const infuraRpcUrl = process.env.INFURA_RPC_URL;
+const infuraMainnetRpcUrl = process.env.INFURA_MAINNET_RPC_URL;
 
 export const publicClient = createPublicClient({
 	batch: {
 		multicall: true,
 	},
-	chain: optimismSepolia,
-	transport: http(alchemyRpcUrl),
+	chain: sepolia,
+	transport: http(infuraRpcUrl),
 });
 
 export const mainnetClient = createPublicClient({
 	chain: mainnet,
-	transport: http(alchemyMainnetRpcUrl),
+	transport: http(infuraMainnetRpcUrl),
 });
 
 export const getMultivaultContract = getContract({
 	address: MULTIVAULT_CONTRACT_ADDRESS,
 	abi: multivaultAbi,
-	publicClient,
+	client: publicClient,
 });
 
 export const multiVaultContract = {
