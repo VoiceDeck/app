f5785b8f32d02b3a0fbe3a9489bacacb585381bd
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-04 16:12:33 -0500

    refactor(reports, my-actions): :truck: Add new routes for My Actions and Reports pages

--- app/routes/reports.tsx
+++ app/routes/reports.tsx
@@ -1,123 +1,122 @@
-import type { LoaderFunction, MetaFunction } from "@remix-run/node";
-import { ClientLoaderFunction, Link, useLoaderData } from "@remix-run/react";
-import { useMemo } from "react";
-import { ConnectButton } from "~/components/connect-button";
-import ReportCard from "~/components/reports/report-card";
-import ReportsHeader from "~/components/reports/reports-header";
-import VoicedeckStats from "~/components/reports/voicedeck-stats";
-import { siteConfig } from "~/config/site";
-import { getNumberOfContributors } from "~/directus.server";
-import { isAuthedUser } from "~/lib/services/auth.server";
-import { Report } from "~/types";
-import { fetchReports } from "../impact-reports.server";
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
+import { Outlet, useFetcher, useLoaderData, useSubmit } from "@remix-run/react";
+import { makeDomainFunction } from "domain-functions";
+import { useEffect, useState } from "react";
+import { User } from "types/user";
+import {
+	sepolia,
+	useAccount,
+	useDisconnect,
+	useNetwork,
+	useSwitchNetwork,
+	useWalletClient,
+} from "wagmi";
+import { z } from "zod";
+import { Footer } from "~/components/global/footer";
+import { NavMenu } from "~/components/global/nav-menu";
+import { isAuthedUser, login } from "~/lib/services/auth.server";
+import { formAction } from "~/lib/services/form.server";
+import { newDIDSessionFromWalletClient } from "~/lib/utils/siwe";
 
-export const meta: MetaFunction = () => {
-	return [
-		{ title: "VoiceDeck" },
-		{ name: "description", content: "Welcome to VoiceDeck!" },
-	];
+const schema = z.object({
+	didSession: z.string(),
+	wallet: z.string(),
+});
+const mutation = makeDomainFunction(schema)(async (values) => {
+	return values;
+});
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+	const resp = await formAction({
+		request,
+		schema,
+		mutation,
+	});
+	if (resp.ok) {
+		await login(request);
+	}
+	return null;
 };
 
-interface IPageData {
-	reports: Report[];
-	numOfContributors: number;
+export async function loader({ request }: LoaderFunctionArgs) {
+	const user = (await isAuthedUser(request)) as User;
+	return json({ user });
 }
 
-export const loader: LoaderFunction = async ({ request }) => {
-	try {
-		const reports = await fetchReports();
-		const numOfContributors = await getNumberOfContributors();
-		const user = await isAuthedUser(request);
+export default function ReportLayout() {
+	const { user } = useLoaderData<typeof loader>();
+	const fetcher = useFetcher();
+	const submit = useSubmit();
 
-		return {
-			user,
-			reports,
-			numOfContributors,
-		};
-	} catch (error) {
-		console.error("Failed to load reports or number of contributors:", error);
-		throw new Response("Failed to load data", { status: 500 });
+	const { isConnected, address } = useAccount();
+	const { chain } = useNetwork();
+	const { switchNetwork } = useSwitchNetwork();
+	const { disconnect } = useDisconnect();
+	const { data: walletClient } = useWalletClient();
+
+	const [hasSigned, setHasSigned] = useState<boolean>(false);
+	const [didSession, setDidSession] = useState<string>("");
+
+	async function signMessage() {
+		try {
+			if (walletClient) {
+				const didSesh = await newDIDSessionFromWalletClient({
+					account: walletClient?.account,
+					signMessage: walletClient?.signMessage,
+				});
+				setDidSession(didSesh.serialize());
+				setHasSigned(true);
+			}
+		} catch (e) {
+			window.alert(e);
+			disconnect();
+			fetcher.submit({}, { method: "post", action: "/actions/auth/logout" });
+		}
 	}
-};
 
-let cacheData: IPageData;
-
-export const clientLoader: ClientLoaderFunction = async ({ serverLoader }) => {
-	if (cacheData) {
-		return {
-			reports: cacheData.reports,
-			numOfContributors: cacheData.numOfContributors,
-		};
+	async function handleSignOut() {
+		setHasSigned(false);
+		disconnect();
+		fetcher.submit({}, { method: "post", action: "/actions/auth/logout" });
 	}
 
-	const serverLoaderData = await serverLoader<IPageData>();
-	const { reports, numOfContributors } = serverLoaderData;
-	cacheData = {
-		reports,
-		numOfContributors,
-	};
-	return cacheData;
-};
+	function handleLogin() {
+		const formData = new FormData();
+		formData.set("didSession", didSession);
+		formData.set("wallet", walletClient?.account?.address as string);
+		submit(formData, {
+			method: "post",
+		});
+	}
 
-clientLoader.hydrate = true;
+	useEffect(() => {
+		if (!user && isConnected && walletClient?.account.address) {
+			signMessage();
+		}
+		if (!isConnected || (isConnected && user && user.wallet !== address)) {
+			if (isConnected) handleSignOut();
+		}
+	}, [user, isConnected, walletClient, address]);
 
-export default function Reports() {
-	const { user, reports, numOfContributors } = useLoaderData<typeof loader>();
+	// Detect Wrong Network
+	useEffect(() => {
+		if (chain !== sepolia && switchNetwork) {
+			switchNetwork(sepolia.id);
+		}
+	}, [chain, switchNetwork]);
 
-	const contributionAmounts = useMemo(() => {
-		const allAmounts = reports.map(
-			(report: Report, index: number) => report.fundedSoFar || 0,
-		);
-		const sumOfAmounts = allAmounts.reduce((a: number, b: number) => a + b, 0);
-		const fullyFunded = allAmounts.filter((amount: number) => amount === 1000);
-		return {
-			amounts: allAmounts,
-			sum: sumOfAmounts,
-			numFunded: fullyFunded.length || 0,
-		};
-	}, [reports]);
+	// Trigger remix auth action if user has a did session and has signed
+	useEffect(() => {
+		if (hasSigned && didSession) {
+			handleLogin();
+		}
+	}, [hasSigned, didSession]);
 
 	return (
 		<>
-			<ConnectButton size="lg" user={user} />
-			<main className="flex flex-col gap-6 md:gap-4 justify-center items-center p-4 md:px-[14%]">
-				<header className="flex-row bg-[url('/hero_imgLG.jpg')] bg-cover bg-center justify-start items-baseline text-vd-beige-200 rounded-3xl p-4 pt-24 md:pt-36 md:pr-48 md:pb-2 md:pl-8 max-w-screen-xl">
-					<h1 className="text-3xl md:text-6xl font-bold text-left">
-						{siteConfig.title}
-					</h1>
-					<h2 className="text-lg font-medium text-left py-6">
-						{siteConfig.description}
-					</h2>
-				</header>
-
-				<VoicedeckStats
-					numOfContributors={numOfContributors}
-					sumOfContributions={contributionAmounts.sum}
-					numOfContributions={contributionAmounts.numFunded}
-				/>
-
-				<ReportsHeader
-					reports={reports}
-					amounts={contributionAmounts.amounts}
-				/>
-
-				<section className="grid grid-rows-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 md:gap-3 max-w-screen-xl">
-					{reports.map((report: Report) => (
-						<Link to={`/reports/${report.slug}`} key={report.hypercertId}>
-							<ReportCard
-								hypercertId={report.hypercertId}
-								image={report.image}
-								title={report.title}
-								summary={report.summary}
-								category={report.category}
-								state={report.state}
-								totalCost={report.totalCost}
-								fundedSoFar={report.fundedSoFar}
-							/>
-						</Link>
-					))}
-				</section>
-			</main>
+			<NavMenu user={user} handleSignout={handleSignOut} />
+			<Outlet />
+			<Footer />
 		</>
 	);
 }
