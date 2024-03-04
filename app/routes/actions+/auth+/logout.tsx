3dcd90244116fbcae9a74999f70889d1d42ee976
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-04 16:11:38 -0500

    refactor(logout.tsx): :recycle: Update logout redirect URL

--- app/routes/actions+/auth+/logout.tsx
+++ app/routes/actions+/auth+/logout.tsx
@@ -3,5 +3,5 @@ import { authenticator } from "~/lib/services/auth.server";
 import type { ActionFunction } from "@remix-run/node";
 
 export const action: ActionFunction = async ({ request }) => {
-	await authenticator.logout(request, { redirectTo: "/login" });
+	await authenticator.logout(request, { redirectTo: "/" });
 };
