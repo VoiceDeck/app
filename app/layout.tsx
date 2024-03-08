2753fe1dbae7a0f18e23b3a73bd152ec8254ed15
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-08 00:16:09 -0500

    feat(shad-cn): :lipstick: Add UI components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import "vaul/dist/index.css"
 import { cn } from "@/lib/utils";
 
 import { cookieToInitialState } from 'wagmi'
