db98cb19bafa21a221533df83f621165d7e101ad
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-07 22:26:46 -0500

    feat: :sparkles: Add web3 modal and connect button

---
+++ wagmi.config.ts
@@ -0,0 +1,32 @@
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { cookieStorage, createStorage } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+
+// Get projectId at https://cloud.walletconnect.com
+export const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
+
+if (!projectId) throw new Error("Project ID is not defined");
+
+// TODO: Add correct data
+const metadata = {
+  name: "VoiceDeck",
+  description: "Funding for Indian journalists",
+  url: "https://app.voicedeck.org", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+// Create wagmiConfig
+export const config = defaultWagmiConfig({
+  chains: [mainnet, sepolia], // required
+  projectId, // required
+  metadata, // required
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  enableWalletConnect: true, // Optional - true by default
+  enableInjected: true, // Optional - true by default
+  enableEIP6963: true, // Optional - true by default
+  enableCoinbase: true, // Optional - true by default
+});
