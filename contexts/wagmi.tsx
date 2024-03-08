1ceb5285172ecd460f803f3a7c048d16aef181bd
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-08 00:21:56 -0500

    refactor: :truck: Move wagmi config and use site config

--- contexts/wagmi.tsx
+++ contexts/wagmi.tsx
@@ -7,7 +7,7 @@ import { createWeb3Modal } from '@web3modal/wagmi/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { State, WagmiProvider } from 'wagmi'
-import { config, projectId } from '@/wagmi.config'
+import { config, projectId } from '@/config/wagmi'
 
 // Setup queryClient
 const queryClient = new QueryClient()
