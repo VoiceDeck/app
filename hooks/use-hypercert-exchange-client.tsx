b0dfd6d0cebcaacdfdd0148394f78b2e23b36314
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-10 20:06:51 -0500

    chore: :recycle: Refactor useHypercertExchangeClient hook to use separate functions for ethers provider and signer

--- hooks/use-hypercert-exchange-client.tsx
+++ hooks/use-hypercert-exchange-client.tsx
@@ -5,6 +5,8 @@ import { getEthersProvider, getEthersSigner } from "@/lib/ethers";
 import { HypercertExchangeClient } from "@hypercerts-org/marketplace-sdk";
 import { useQuery } from "@tanstack/react-query";
 import { useAccount, useConfig, useWalletClient } from "wagmi";
+import { useEthersProvider } from "./use-ethers-provider";
+import { useEthersSigner } from "./use-ethers-signer";
 
 const isSupportedChain = (chainId: number) => {
 	const supportedChainIds = [10, 42220, 11155111]; // Replace with actual chain IDs
@@ -19,23 +21,20 @@ export const useHypercertExchangeClient = ({
 } = {}) => {
 	const { chain } = useAccount();
 
-	const { data: signer } = useQuery({
-		queryKey: ["signer"],
-		queryFn: () =>
-			getEthersSigner(config, { chainId: chain?.id }).then((signer) => signer),
-		enabled: !!chain?.id,
-	});
+	const signer = useEthersSigner();
+	const provider = useEthersProvider();
 
 	const config = useConfig();
 	const clientConfig = {
 		chain: overrideChainId ? { id: overrideChainId } : { id: chain?.id },
-		provider: getEthersProvider(config, { chainId: chain?.id }),
+		provider,
 		signer,
 	};
 	const [client, setClient] = useState<HypercertExchangeClient | null>(() => {
 		if (clientConfig.chain?.id && isSupportedChain(clientConfig.chain?.id)) {
 			return new HypercertExchangeClient(
 				clientConfig.chain.id,
+				// @ts-ignore
 				clientConfig.provider,
 				clientConfig.signer,
 
@@ -70,10 +69,11 @@ export const useHypercertExchangeClient = ({
 					chain: { id: chainId },
 					// walletClient: walletClient as any,
 				};
-
 				const client = new HypercertExchangeClient(
 					config.chain?.id,
+					// @ts-ignore
 					config.provider,
+					config.signer,
 				);
 				setClient(client);
 			} catch (e) {
