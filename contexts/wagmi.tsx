a639dbf7adc94ddecd7e5d78e9ace1e67cb6e5be
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-11 17:23:39 -0500

    style(wagmi): :art: Fix import statements and formatting in wagmi.tsx

--- contexts/wagmi.tsx
+++ contexts/wagmi.tsx
@@ -1,36 +1,36 @@
-'use client'
+"use client";
 
-import React, { ReactNode } from 'react'
+import React, { type ReactNode } from "react";
 
-import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { createWeb3Modal } from "@web3modal/wagmi/react";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { State, WagmiProvider } from 'wagmi'
-import { config, projectId } from '@/config/wagmi'
+import { config, projectId } from "@/config/wagmi";
+import { type State, WagmiProvider } from "wagmi";
 
 // Setup queryClient
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-if (!projectId) throw new Error('Project ID is not defined')
+if (!projectId) throw new Error("Project ID is not defined");
 
 // Create modal
 createWeb3Modal({
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: true // Optional - defaults to your Cloud configuration
-})
+	wagmiConfig: config,
+	projectId,
+	enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
 
 export function WagmiContextProvider({
-  children,
-  initialState
+	children,
+	initialState,
 }: {
-  children: ReactNode
-  initialState?: State
+	children: ReactNode;
+	initialState?: State;
 }) {
-  return (
-    <WagmiProvider config={config} initialState={initialState}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </WagmiProvider>
-  )
+	return (
+		<WagmiProvider config={config} initialState={initialState}>
+			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		</WagmiProvider>
+	);
 }
