85f77eb2bb0b1e557e8dd710aa01273e46f454f5
Rowdy <mroddy5280@gmail.com>
2024-03-13 22:17:02 -0600

    feat(nav,-profile,-connect-button): updated nav to move client side functionality to connect button. restored the my-settings page as profile page. added api call for user data based on wallet address

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,24 +2,37 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export function formatDate(d: Date) {
-	const date = new Date(d);
-	return date
-		.toLocaleDateString("en-US", {
-			year: "numeric",
-			month: "short", // "Oct"
-			day: "numeric",
-		})
-		.toUpperCase();
+  const date = new Date(d);
+  return date
+    .toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short", // "Oct"
+      day: "numeric",
+    })
+    .toUpperCase();
 }
 
 export const formatCurrency = (value: number, currencyCode = "USD") => {
-	return new Intl.NumberFormat("en-US", {
-		style: "currency",
-		currency: currencyCode,
-		minimumFractionDigits: 2,
-	}).format(value);
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currencyCode,
+    minimumFractionDigits: 2,
+  }).format(value);
+};
+
+export const truncateEthereumAddress = (
+  address: `0x${string}`,
+  length: number = 4,
+): string => {
+  if (!address) {
+    return "";
+  }
+  if (address.length <= 2 + length * 2) {
+    return address;
+  }
+  return `${address.substring(0, length + 2)}...${address.substring(address.length - length)}`;
 };
