076bec2383a81356a4dca4f29dd39ab7e4d03644
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-15 10:22:43 -0500

    refactor(filter-sidebar): :lipstick: Update filter sidebar styling and icons

--- components/reports/filter-sidebar.tsx
+++ components/reports/filter-sidebar.tsx
@@ -1,7 +1,7 @@
 import { useFilters } from "@/contexts/filter";
 import type { createFilterOptions } from "@/lib/search-filter-utils";
 import { cn } from "@/lib/utils";
-import { X } from "lucide-react";
+import { RewindIcon } from "lucide-react";
 import { FilterItems } from "./reports-filters";
 
 interface SidebarFilterProps {
@@ -21,7 +21,7 @@ const SidebarFilter = ({
 	return (
 		<aside
 			className={cn(
-				"w-[350px] p-5 bg-vd-beige-100 border-r border-r-stone-300 transition-[margin-left] ease-[cubic-bezier(0.245, -0.045, 0.100, 1.145)] duration-500",
+				"w-[350px] p-5 bg-vd-beige-100 border-r border-r-stone-300 transition-[margin-left] ease-[cubic-bezier(0.65, 0, 0.35, 1)] duration-700",
 				isOpen ? "ml-0" : "ml-[-350px]",
 			)}
 		>
@@ -30,13 +30,19 @@ const SidebarFilter = ({
 				<section className="flex gap-4">
 					<button
 						type="button"
-						className="text-vd-blue-700"
+						className="text-vd-blue-700 hover:text-vd-blue-400 hover:underline underline-offset-1"
 						onClick={() => updateSearchParams(newFilters)}
 					>
 						Clear filters
 					</button>
-					<button type="button" onClick={() => setIsOpen(false)}>
-						<X size={24} />
+					<button
+						type="button"
+						onClick={() => setIsOpen(false)}
+						title="Close sidebar"
+						aria-label="Close sidebar"
+						className="hover:-translate-x-1 transition-transform ease-in-out duration-300"
+					>
+						<RewindIcon size={28} />
 					</button>
 				</section>
 			</header>
