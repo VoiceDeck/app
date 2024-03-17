2fdecd3da1586357dc1e9f2ddeb7872e19ac4f7a
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-17 14:45:48 -0500

    chore(next.config.mjs): :wrench: Add domain for images

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
 		config.externals.push("pino-pretty", "lokijs", "encoding");
 		return config;
 	},
+	images: {
+		domains: ["directus.vd-dev.org"],
+	},
 	redirects: async () => {
 		return [
 			{
