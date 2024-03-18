1423bde95d885aa3a992671fc276e133b6b01629
Rowdy <mroddy5280@gmail.com>
2024-03-18 17:24:38 -0600

    feat(not-found.tsx): created page for 404 page for site

---
+++ app/not-found.tsx
@@ -0,0 +1,24 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+	return (
+		<section className="flex flex-col justify-center items-center gap-4 py-8">
+			<h1 className="text-5xl font-extrabold">Oops!</h1>
+			<Image src="/teapot.svg" alt="404" width={300} height={300} />
+			<h2 className="text-4xl">Page Not Found</h2>
+			<p className="text-xl">Giving it more time to blossom</p>
+			<Link
+				className={cn(
+					buttonVariants({ variant: "default" }),
+					"rounded-md font-semibold",
+				)}
+				href="/"
+			>
+				Return Home
+			</Link>
+		</section>
+	);
+}
