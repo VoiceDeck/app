d699e662769baa22ffb0330fa7cdf3e27b521cb8
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-27 16:59:53 -0500

    refactor(profile settings): :lipstick: Update profile settings layout
    Add button and tidy up layout

--- app/profile/[address]/settings/page.tsx
+++ app/profile/[address]/settings/page.tsx
@@ -1,5 +1,7 @@
 import { UserInfo } from "@/components/settings/user-info";
 import { VerifiedCitizen } from "@/components/settings/verified-citizen";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function SettingsPage({
 	params: { address },
@@ -7,8 +9,21 @@ export default function SettingsPage({
 	params: { address: `0x${string}` };
 }) {
 	return (
-		<main className="container mx-auto flex flex-col gap-4 text-vd-blue-900 mb-6 max-w-3xl pb-16 md:pb-0">
-			<header className="md:col-span-3 flex justify-between pt-4">
+		<main className="p-4 md:p-8 md:max-w-screen-xl mx-auto flex flex-col gap-4 text-vd-blue-900 mb-6 max-w-3xl pb-16 md:pb-0">
+			<header className="md:col-span-3 pt-4">
+				<Link
+					href={`/profile/${address}`}
+					className="group flex space-x-1 items-center"
+				>
+					<ChevronLeft
+						size={24}
+						className="text-vd-blue-400 group-hover:-translate-x-2 transition-transform duration-300 ease-in-out"
+					/>
+					<p className="font-semibold text-sm uppercase text-vd-blue-500 tracking-wider">
+						Profile
+					</p>
+				</Link>
+				<div className="p-2" />
 				<h1 className="text-3xl md:text-4xl font-semibold">Settings</h1>
 			</header>
 			<UserInfo />
--- components/settings/verified-citizen.tsx
+++ components/settings/verified-citizen.tsx
@@ -1,39 +1,33 @@
 import { HelpCircleIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
 import { VerifiedStatus } from "@/components/settings/verified-status";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
 const VerifiedCitizen = () => {
-  return (
-    <section className="flex flex-col gap-4">
-      <h2 className="text-xl md:text-2xl font-semibold md:py-2">
-        Verified Citizen
-      </h2>
-      <Card className="bg-vd-blue-200 rounded-3xl flex-1 shadow-none border-none">
-        <CardHeader>
-          <CardTitle className={cn("flex gap-1 items-center pb-0")}>
-            <HelpCircleIcon size={16} strokeWidth={2} />
-            Why does this matter?
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-sm">
-            We prioritize the authenticity of each report. Recognizing that
-            local residents have the most accurate insights into matters
-            affecting their own region, we want to give the power back to them.
-            Using an advanced cryptographic developer tool known as Anon
-            Aadhaar, we use Aadhaar’s QR code to verify an individual's
-            citizenship without revealing any information about them. For
-            further details, you can read more here.
-          </p>
-        </CardContent>
-      </Card>
-      <Separator />
-      <VerifiedStatus />
-    </section>
-  );
+	return (
+		<section className="flex flex-col gap-4 max-w-lg">
+			<h2 className="text-xl md:text-2xl font-semibold md:py-2">
+				Citizen Verification
+			</h2>
+			<Card className="bg-vd-blue-200 rounded-md flex-1 shadow-none border-none">
+				<CardHeader>
+					<CardTitle className={cn("flex gap-1 items-center pb-0")}>
+						<HelpCircleIcon size={16} strokeWidth={2} />
+						Why does this matter?
+					</CardTitle>
+				</CardHeader>
+				<CardContent>
+					Our focus is on report authenticity, empowering local residents with
+					the most accurate regional insights. Through Anon Aadhaar, a
+					sophisticated cryptographic tool, we verify citizenship using
+					Aadhaar’s QR code, ensuring privacy. For more information, read here.
+				</CardContent>
+			</Card>
+			<VerifiedStatus />
+		</section>
+	);
 };
 
 VerifiedCitizen.displayName = "VerifiedCitizen";
