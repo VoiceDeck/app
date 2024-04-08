27caa9dceac03a1439091ef50f5cf66e1a52fa2f
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-04-08 17:30:38 -0500

    fix: :recycle: Add AnonAadhaarCard component in profile sidebar

--- components/profile/sidebar.tsx
+++ components/profile/sidebar.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import {
-	AnonAadhaarProof,
 	AnonAadhaarProvider,
 	LogInWithAnonAadhaar,
 	useAnonAadhaar,
 } from "@anon-aadhaar/react";
-import { useEffect, useState } from "react";
 
 import { buttonVariants } from "@/components/ui/button";
 import {
@@ -16,43 +14,36 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { ArrowUpRight, CheckCircle2 } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
-const SideBar = () => {
+const AnonAadhaarCard = () => {
 	const [anonAadhaar] = useAnonAadhaar();
 
-	useEffect(() => {
-		console.log("Anon Aadhaar status: ", anonAadhaar.status);
-	}, [anonAadhaar.status]);
-
 	return (
-		<section className="flex flex-col gap-4 md:col-span-1 md:row-span-2">
-			<Card className="bg-vd-beige-100 rounded-3xl shadow-none border-none">
-				<AnonAadhaarProvider _useTestAadhaar={true}>
-					<CardHeader>
-						<img
-							src="/teapot.svg"
-							alt="teapot that looks like a building"
-							className="mx-auto px-6 w-40 h-auto my-4"
-						/>
-						<CardTitle className="text-center text-lg md:text-xl">
-							{anonAadhaar.status === "logged-in"
-								? "You're a local advocate!"
-								: "Log in to become a local advocate"}
-						</CardTitle>
-						<CardDescription className="text-center text-base">
-							{anonAadhaar.status === "logged-in"
-								? "Verified citizens can evaluate impacts and vote on VoiceDeck, shaping their community's future."
-								: "Verify with Aadhaar to access community impact evaluation and voting on VoiceDeck."}
-						</CardDescription>
-					</CardHeader>
-					<CardContent>
-						<div className="flex flex-col gap-2 w-full px-2">
-							<div className="flex flex-col gap-1 justify-center items-center w-full">
-								<LogInWithAnonAadhaar nullifierSeed={893772993} />
-								{/* {anonAadhaar.status === "logged-in" && (
+		<Card className="bg-vd-beige-100 rounded-3xl shadow-none border-none">
+			<CardHeader>
+				<img
+					src="/teapot.svg"
+					alt="teapot that looks like a building"
+					className="mx-auto px-6 w-40 h-auto my-4"
+				/>
+				<CardTitle className="text-center text-lg md:text-xl">
+					{anonAadhaar.status === "logged-in"
+						? "You're a local advocate!"
+						: "Log in to become a local advocate"}
+				</CardTitle>
+				<CardDescription className="text-center text-base">
+					{anonAadhaar.status === "logged-in"
+						? "Verified citizens can evaluate impacts and vote on VoiceDeck, shaping their community's future."
+						: "Verify with Aadhaar to access community impact evaluation and voting on VoiceDeck."}
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<div className="flex flex-col gap-2 w-full px-2">
+					<div className="flex flex-col gap-1 justify-center items-center w-full">
+						<LogInWithAnonAadhaar nullifierSeed={893772993} />
+						{/* {anonAadhaar.status === "logged-in" && (
                   <Alert className="flex justify-center w-full gap-1 bg-stone-100">
                     <CheckCircle2 size={24} className="text-green-600" />
                     <p className="text-center text-vd-blue-900">
@@ -60,26 +51,34 @@ const SideBar = () => {
                     </p>
                   </Alert>
                 )} */}
-							</div>
-							<a
-								href="https://voicedeck.org/faq#anonaadhaar"
-								target="_blank"
-								rel="noopener noreferrer"
-								className={`${buttonVariants({
-									variant: "link",
-								})} font-semibold group`}
-							>
-								<span>Learn more</span>
-								<ArrowUpRight
-									size={18}
-									className="ml-1 opacity-70 group-hover:translate-x-0.5 group-hover:opacity-100 group-hover:-translate-y-0.5 transition-transform duration-300 ease-in-out"
-									aria-hidden="true"
-								/>
-							</a>
-						</div>
-					</CardContent>
-				</AnonAadhaarProvider>
-			</Card>
+					</div>
+					<a
+						href="https://voicedeck.org/faq#anonaadhaar"
+						target="_blank"
+						rel="noopener noreferrer"
+						className={`${buttonVariants({
+							variant: "link",
+						})} font-semibold group`}
+					>
+						<span>Learn more</span>
+						<ArrowUpRight
+							size={18}
+							className="ml-1 opacity-70 group-hover:translate-x-0.5 group-hover:opacity-100 group-hover:-translate-y-0.5 transition-transform duration-300 ease-in-out"
+							aria-hidden="true"
+						/>
+					</a>
+				</div>
+			</CardContent>
+		</Card>
+	);
+};
+
+const SideBar = () => {
+	return (
+		<section className="flex flex-col gap-4 md:col-span-1 md:row-span-2">
+			<AnonAadhaarProvider _useTestAadhaar={true}>
+				<AnonAadhaarCard />
+			</AnonAadhaarProvider>
 			<Card className="rounded-3xl bg-vd-beige-100 shadow-none border-none">
 				<CardHeader>
 					<img
