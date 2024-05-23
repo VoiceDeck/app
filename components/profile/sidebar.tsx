a3967ed2237a8e085042475cbbff97a467e6de61
Rowdy <mroddy5280@gmail.com>
2024-05-23 15:55:51 -0600

    feat(anon-aadhaar): remove anon-aadhaar from package. remove component from profile page

--- components/profile/sidebar.tsx
+++ components/profile/sidebar.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import {
-	AnonAadhaarProvider,
-	LogInWithAnonAadhaar,
-	useAnonAadhaar,
-} from "@anon-aadhaar/react";
-
 import { buttonVariants } from "@/components/ui/button";
 import {
 	Card,
@@ -17,60 +11,58 @@ import {
 import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
-const AnonAadhaarCard = () => {
-	const [anonAadhaar] = useAnonAadhaar();
+// TODO: Remove as this is not used
+// const AnonAadhaarCard = () => {
+// 	const [anonAadhaar] = useAnonAadhaar();
 
-	return (
-		<Card className="bg-vd-beige-100 rounded-3xl shadow-none border-none">
-			<CardHeader>
-				<img
-					src="/teapot.svg"
-					alt="teapot that looks like a building"
-					className="mx-auto px-6 w-40 h-auto my-4"
-				/>
-				<CardTitle className="text-center text-lg md:text-xl">
-					{anonAadhaar.status === "logged-in"
-						? "You're a local advocate!"
-						: "Log in to become a local advocate"}
-				</CardTitle>
-				<CardDescription className="text-center text-base">
-					{anonAadhaar.status === "logged-in"
-						? "Verified citizens can evaluate impacts and vote on VoiceDeck, shaping their community's future."
-						: "Verify with Aadhaar to access community impact evaluation and voting on VoiceDeck."}
-				</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<div className="flex flex-col gap-2 w-full px-2">
-					<div className="flex flex-col gap-1 justify-center items-center w-full">
-						<LogInWithAnonAadhaar nullifierSeed={893772993} />
-					</div>
-					<a
-						href="https://voicedeck.org/faq#anonaadhaar"
-						target="_blank"
-						rel="noopener noreferrer"
-						className={`${buttonVariants({
-							variant: "link",
-						})} font-semibold group`}
-					>
-						<span>Learn more</span>
-						<ArrowUpRight
-							size={18}
-							className="ml-1 opacity-70 group-hover:translate-x-0.5 group-hover:opacity-100 group-hover:-translate-y-0.5 transition-transform duration-300 ease-in-out"
-							aria-hidden="true"
-						/>
-					</a>
-				</div>
-			</CardContent>
-		</Card>
-	);
-};
+// 	return (
+// 		<Card className="bg-vd-beige-100 rounded-3xl shadow-none border-none">
+// 			<CardHeader>
+// 				<img
+// 					src="/teapot.svg"
+// 					alt="teapot that looks like a building"
+// 					className="mx-auto px-6 w-40 h-auto my-4"
+// 				/>
+// 				<CardTitle className="text-center text-lg md:text-xl">
+// 					{anonAadhaar.status === "logged-in"
+// 						? "You're a local advocate!"
+// 						: "Log in to become a local advocate"}
+// 				</CardTitle>
+// 				<CardDescription className="text-center text-base">
+// 					{anonAadhaar.status === "logged-in"
+// 						? "Verified citizens can evaluate impacts and vote on VoiceDeck, shaping their community's future."
+// 						: "Verify with Aadhaar to access community impact evaluation and voting on VoiceDeck."}
+// 				</CardDescription>
+// 			</CardHeader>
+// 			<CardContent>
+// 				<div className="flex flex-col gap-2 w-full px-2">
+// 					<div className="flex flex-col gap-1 justify-center items-center w-full">
+// 						<LogInWithAnonAadhaar nullifierSeed={893772993} />
+// 					</div>
+// 					<a
+// 						href="https://voicedeck.org/faq#anonaadhaar"
+// 						target="_blank"
+// 						rel="noopener noreferrer"
+// 						className={`${buttonVariants({
+// 							variant: "link",
+// 						})} font-semibold group`}
+// 					>
+// 						<span>Learn more</span>
+// 						<ArrowUpRight
+// 							size={18}
+// 							className="ml-1 opacity-70 group-hover:translate-x-0.5 group-hover:opacity-100 group-hover:-translate-y-0.5 transition-transform duration-300 ease-in-out"
+// 							aria-hidden="true"
+// 						/>
+// 					</a>
+// 				</div>
+// 			</CardContent>
+// 		</Card>
+// 	);
+// };
 
 const SideBar = () => {
 	return (
 		<section className="flex flex-col gap-4 md:col-span-1 md:row-span-2">
-			<AnonAadhaarProvider _useTestAadhaar={true}>
-				<AnonAadhaarCard />
-			</AnonAadhaarProvider>
 			<Card className="rounded-3xl bg-vd-beige-100 shadow-none border-none">
 				<CardHeader>
 					<img
--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack: (config) => {
-		config.externals.push("pino-pretty", "lokijs", "encoding");
-		return config;
-	},
-	images: {
-		remotePatterns: [
-			{
-				hostname: process.env.NEXT_PUBLIC_DEPLOY_ENV === "production" ? "directus.voicedeck.org" : "directus.vd-dev.org",
-				protocol: "https",
-			}
-		]
-	},
-	redirects: async () => {
-		return [
-			{
-				source: "/",
-				destination: "/reports",
-				permanent: true,
-			},
-				{
-				source: "/profile",
-				destination: "/reports",
-				permanent: true,
-			},
-		];
-	},
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname:
+          process.env.NEXT_PUBLIC_DEPLOY_ENV === "production"
+            ? "directus.voicedeck.org"
+            : "directus.vd-dev.org",
+        protocol: "https",
+      },
+      {
+        hostname: "directus.voicedeck.org",
+        protocol: "https",
+      },
+    ],
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/reports",
+        permanent: true,
+      },
+      {
+        source: "/profile",
+        destination: "/reports",
+        permanent: true,
+      },
+    ];
+  },
 };
-
 
 export default nextConfig;
