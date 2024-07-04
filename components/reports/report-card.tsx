cc2ad25be020a74d34faf31a2822984aecb65598
Rowdy <mroddy5280@gmail.com>
2024-07-04 01:55:51 -0600

    feat(report-card): updated to new grey background style

--- components/reports/report-card.tsx
+++ components/reports/report-card.tsx
@@ -31,9 +31,9 @@ const ReportCard: React.FC<ExploreHypercertCardProps> = ({
 	const percentAvailable = calculateBigIntPercentage(units, totalUnitsForSale);
 	return (
 		<Link href={`/${hypercert_id}`} passHref>
-			<article className="hover:-translate-y-2 group relative overflow-hidden rounded-md transition-transform duration-300">
-				<div className="h-[320px] min-w-[300px] max-w-[350px]">
-					<div className="relative h-full w-full overflow-hidden bg-black">
+			<article className="hover:-translate-y-2 group relative overflow-hidden rounded-lg bg-black/10 transition-transform duration-300">
+				<div className="h-[320px] min-w-[300px] max-w-[320px]">
+					<div className="relative h-full w-full">
 						<Image
 							// src={`/api/hypercerts/${hypercert_id}/image`}
 							src={image}
@@ -45,22 +45,22 @@ const ReportCard: React.FC<ExploreHypercertCardProps> = ({
 					</div>
 				</div>
 				<section className="absolute top-4 left-4 flex space-x-1 opacity-100 transition-opacity duration-150 ease-out group-hover:opacity-100 md:opacity-0">
-					<div className="rounded-md border border-white/60 bg-black px-2 py-0.5 text-white text-xs">
+					<div className="rounded-md border border-white/60 bg-black px-2 py-0.5 text-white text-xs shadow-sm">
 						{cardChain(chain_id)}
 					</div>
-					<div className="rounded-md border border-white/60 bg-black px-2 py-0.5 text-white text-xs">
+					<div className="rounded-md border border-black/60 bg-black px-2 py-0.5 text-white text-xs shadow-sm">
 						approved
 					</div>
 				</section>
-				<section className="absolute bottom-0 w-full space-y-2 bg-black/70 p-4 text-white backdrop-blur-sm">
+				<section className="absolute bottom-0 w-full space-y-2 bg-gray-200/80 p-4 text-black backdrop-blur-md">
 					<p
-						className={`line-clamp-2 flex-1 text-ellipsis font-medium text-sm${
-							name ? "text-white" : "text-slate-100"
+						className={`line-clamp-2 flex-1 text-ellipsis font-semibold text-sm${
+							name ? "text-black" : "text-gray-700"
 						}`}
 					>
 						{name || "[Untitled]"}
 					</p>
-					<Separator className="my-2 border-white opacity-40" />
+					<Separator className="my-2 bg-black/40" />
 					<section className="flex justify-between text-xs">
 						<section>
 							<h6 className="opacity-70">for sale</h6>
