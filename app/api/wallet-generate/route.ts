705bc2fa93fb50bc31aa9bd1caa4f905e2c27704
dipanshuhappy <dipanshuhappy@gmail.com>
2024-11-07 16:47:09 +0700

    chore: update @privy-io/server-auth to version 1.16.1 and comment out wallet generation code

--- app/api/wallet-generate/route.ts
+++ app/api/wallet-generate/route.ts
@@ -1,33 +1,32 @@
-import { PrivyClient } from "@privy-io/server-auth";
-import { type NextRequest, NextResponse } from "next/server";
+// import { PrivyClient } from "@privy-io/server-auth";
+// import { type NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-	const privy = new PrivyClient(
-		process.env.PRIVY_APP_ID ?? "",
-		process.env.PRIVY_APP_SECRET ?? "",
-	);
-	console.log(process.env.PRIVY_APP_ID, process.env.PRIVY_APP_SECRET);
-	console.log({ privy });
-	const { address } = await req.json();
-	console.log("address", address);
-
-	if (!address) {
-		console.log("Missing required fields");
-		return NextResponse.json(
-			{ error: "Missing required fields" },
-			{ status: 400 },
-		);
-	}
-
-	const wallet = await privy.importUser({
-		linkedAccounts: [
-			{
-				type: "email",
-				address: address,
-			},
-		],
-		createEthereumWallet: true,
-	});
+// export async function POST(req: NextRequest) {
+// 	const privy = new PrivyClient(
+// 		process.env.PRIVY_APP_ID ?? "",
+// 		process.env.PRIVY_APP_SECRET ?? "",
+// 	);
 	
-	return NextResponse.json({ wallet }, { status: 200 });
-}
+// 	const { address } = await req.json();
+// 	console.log("address", address);
+
+// 	if (!address) {
+// 		console.log("Missing required fields");
+// 		return NextResponse.json(
+// 			{ error: "Missing required fields" },
+// 			{ status: 400 },
+// 		);
+// 	}
+
+// 	const wallet = await privy.importUser({
+// 		linkedAccounts: [
+// 			{
+// 				type: "email",
+// 				address: address,
+// 			},
+// 		],
+// 		createEthereumWallet: true,
+// 	});
+	
+// 	return NextResponse.json({ wallet }, { status: 200 });
+// }
